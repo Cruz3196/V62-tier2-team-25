00b1b3805f117a633e74d128c34225308e3866bd
Damaris <damaris@MacBook-Pro-de-Bertha.local>
2026-09-08 10:44:36 -0700

    feat: Add DevTrajectory header component to layout

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import LandingPage from "./components/LandingPage";
+import Header from './components/Header';
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Header />
       <LandingPage />
     </>
   );
