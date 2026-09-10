7df729caafeced42df235eca4ee610f2cd7a722e
Damaris <damaris@MacBook-Pro-de-Bertha.local>
2026-09-09 18:58:41 -0700

    fix: remove duplicate LandingPage import in App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,6 @@ import LandingPage from "./components/LandingPage";
 import { Footer } from "./components/layout/footer";
 import Header from './components/Header';
 import "./App.css";
-import LandingPage from "./components/LandingPage";
 import AboutLPG from "./components/AboutLPG";
 
 function App() {
